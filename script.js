e4d208e45d35415a4434007227d070f10c9b692a
an154h <anisahisk@hotmail.com>
2026-09-01 14:46:44 +0800

    add: add carousel feature for project display

--- script.js
+++ script.js
@@ -30,3 +30,13 @@ if (codePreview && previewButton) {
     previewButton.classList.toggle('is-playing', isPlaying);
   });
 }
+
+const carousel = document.querySelector('.carousel-container');
+const slide = document.querySelector('.carousel-slide');
+
+function handleCarouselMove(positive = true) {
+  const slideWidth = slide.clientWidth;
+  carousel.scrollLeft = positive ?
+  carousel.scrollLeft + slideWidth :
+  carousel.scrollLeft - slideWidth;
+}
